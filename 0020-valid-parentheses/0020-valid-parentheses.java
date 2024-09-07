d29ae9d30225f8c7351efd2c27b8e9c742ddd21b
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-07 21:45:13 +0200

    Time: 1 ms (98.69%), Space: 41.7 MB (17.29%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+
+        Stack<Character> myStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            if (currChar == '(' || currChar == '[' || currChar == '{') {
+                myStack.push(currChar);
+                continue;
+            }
+
+            if (!myStack.isEmpty()) {
+                switch(currChar) {
+                    case ')':
+                        if (myStack.pop() != '(') return false;
+                        break;
+                    
+                    case ']':
+                        if (myStack.pop() != '[') return false;
+                        break;
+
+                    case '}':
+                        if (myStack.pop() != '{') return false;
+                        break;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if (myStack.isEmpty()) 
+            return true;
+        else 
+            return false;
+    }
+}
