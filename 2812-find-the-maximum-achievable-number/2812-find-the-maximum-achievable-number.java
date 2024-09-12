d1b2edf48ddbe05a3c743e75b0abdd785b827e51
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-12 10:21:20 +0200

    Time: 1 ms (100%), Space: 41.3 MB (74.17%) - LeetHub

--- 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.java
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.java
@@ -1,11 +1,8 @@
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
-        //int maxAchievableNumber = Integer.MIN_VALUE;
         for (int i = 0; i < t; i++) {
-            //maxAchievableNumber = num + 2;
             num += 2;
         }
-        //return maxAchievableNumber;
         return num;
     }
 }
