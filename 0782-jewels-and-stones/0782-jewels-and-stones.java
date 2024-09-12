7ccec1748f442bfb4dd9d54d04dfad108fc5b11f
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-12 23:32:11 +0200

    Time: 0 ms (100%), Space: 41.7 MB (47.32%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,14 +1,9 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> map = new HashMap<>();
         int ans = 0;
 
-        for (int i = 0; i < jewels.length(); i++) {
-            map.put(jewels.charAt(i), 0);
-        }
-
         for (int i = 0; i < stones.length(); i++) {
-            if (map.containsKey(stones.charAt(i))) {
+            if (jewels.indexOf(stones.charAt(i)) != -1) {
                 ans += 1;
             }
         }
