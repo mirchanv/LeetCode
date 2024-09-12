969ad5e6e048096105d5eb6be15f755cbbb27edc
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-12 23:52:19 +0200

    Time: 0 ms (100%), Space: 44.5 MB (79.6%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int x = 0;
+        int y = n;
+
+        for (int i = 0; i < nums.length; i+=2) {
+            ans[i] = nums[x];
+            ans[i + 1] = nums[y];
+            x++;
+            y++;
+        }
+
+        return ans;
+    }
+}
