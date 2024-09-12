2abc8446064bf49e53459fdeaf4dce8e6976e165
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-12 16:46:50 +0200

    Time: 1 ms (87.82%), Space: 41.2 MB (17.74%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -4,7 +4,9 @@ class Solution {
 
         for (int i = 0; i < nums.length-1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) pairs++;
+                if (nums[i] == nums[j]) {
+                    pairs++;
+                }
             }
         }
 
