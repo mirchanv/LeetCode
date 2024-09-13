2a0b98c9924e0ea9ce9f6266bcf9923b07cda8da
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-13 13:33:41 +0200

    Time: 0 ms (100%), Space: 40.2 MB (67.32%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -2,10 +2,10 @@ class Solution {
     public int smallestEvenMultiple(int n) {
         if (n % 2 == 0) return n;
 
-        int temp = n + n;
-        while (temp % 2 != 0) {
-            temp++; 
+        n = n + n;
+        while (n % 2 != 0) {
+            n = n + n; 
         }
-        return temp;
+        return n;
     }
 }
