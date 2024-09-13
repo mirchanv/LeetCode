4bf2f79af04d0f1f2b0767d12ac5ee16ba0009f4
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-13 09:17:26 +0200

    Time: 0 ms (100%), Space: 42.3 MB (97.84%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for (int[] curr : accounts) {
+            int customerWealth = 0;
+            for (int val : curr) {
+                customerWealth += val;
+            }
+            maxWealth = Math.max(maxWealth, customerWealth);
+        }
+
+        return maxWealth;
+    }
+}
