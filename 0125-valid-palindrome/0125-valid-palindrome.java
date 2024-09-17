18ecd33771927a86b8c7a2c968f6772110b272a9
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-18 00:57:08 +0200

    Time: 3 ms (73.1%), Space: 42.8 MB (76.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.length() <= 1) return true;
-
-        //s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
-        
         int left = 0;
         int right = s.length()-1;
         
