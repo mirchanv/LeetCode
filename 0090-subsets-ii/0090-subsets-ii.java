63ea8c950d42d88e6fb9dd25be8da7bb8cbe8a25
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2024-09-22 00:33:08 +0200

    Time: 1 ms (99.92%), Space: 43.7 MB (22.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,9 +1,7 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        //boolean[] visited = new boolean[nums.length];
         Arrays.sort(nums);
-
         getSubsets(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
@@ -14,13 +12,10 @@ class Solution {
         if (index >= arr.length) return;
 
         for (int i = index; i < arr.length; i++) {
-            // if(visited[i]) continue;
             if(i > index && arr[i] == arr[i-1]) continue;
             ans.add(arr[i]);
-            // visited[i] = true;
             getSubsets(arr, i + 1, ans, list);
             ans.removeLast();
-            // visited[i] = false;
         }
     }
 }
