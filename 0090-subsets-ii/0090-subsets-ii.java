7cd49a58356ceddfe5e749b60e07966cc8cbd30a
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2024-09-22 00:32:22 +0200

    Time: 1 ms (99.92%), Space: 43.9 MB (7.72%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
+        //boolean[] visited = new boolean[nums.length];
         Arrays.sort(nums);
 
-        getSubsets(nums, 0, visited, new ArrayList<>(), ans);
+        getSubsets(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
 
-    public void getSubsets(int[] arr, int index, boolean[] visited, List<Integer> ans, List<List<Integer>> list) {
+    public void getSubsets(int[] arr, int index, List<Integer> ans, List<List<Integer>> list) {
         list.add(new ArrayList(ans));
 
         if (index >= arr.length) return;
 
-        for (int i = index; i < visited.length; i++) {
-            if(visited[i]) continue;
+        for (int i = index; i < arr.length; i++) {
+            // if(visited[i]) continue;
             if(i > index && arr[i] == arr[i-1]) continue;
             ans.add(arr[i]);
-            visited[i] = true;
-            getSubsets(arr, i + 1, visited, ans, list);
+            // visited[i] = true;
+            getSubsets(arr, i + 1, ans, list);
             ans.removeLast();
-            visited[i] = false;
+            // visited[i] = false;
         }
     }
 }
