ab52924b45c5b7edfc2a138543e1a22d75b2b256
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-12 14:44:34 +0200

    Time: 43 ms (86.19%), Space: 55.2 MB (59.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let xCopy = x;
-    xCopy = Math.abs(xCopy);
-    let rev = 0;
-    
-    while(xCopy > 0) {
-        let lastDigit = xCopy % 10;
-        rev = (10 * rev) + lastDigit;
-        xCopy = Math.floor(xCopy / 10);
-    }
-    
-    const limit = Math.pow(2, 31);
-    if (-rev < -limit || rev > limit-1) return 0;
+    let xCopy = Math.abs(x);
+  let rev = 0;
 
-    return x < 0 ? -rev : rev;
+  while (xCopy > 0) {
+    let lastDigit = xCopy % 10;
+    rev = 10 * rev + lastDigit;
+    xCopy = Math.floor(xCopy / 10);
+  }
 
+  const limit = Math.pow(2, 31);
+  if (-rev < -limit || rev > limit - 1) return 0;
+
+  return x < 0 ? -rev : rev;
 };
