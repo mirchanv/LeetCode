ec1e6dbea3b851e1ea2579967eceb85bc0c0a56d
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-14 13:01:01 +0200

    Time: 0 ms (100%), Space: 54 MB (26.79%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -6,11 +6,10 @@
 var removeElement = function(nums, val) {
     let x = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != val) {
+        if (nums[i] !== val) {
             let temp = nums[i];
             nums[i] = nums[x];
-            nums[x] = temp;
-            x++;
+            nums[x++] = temp;
         }
     }
     return x;
