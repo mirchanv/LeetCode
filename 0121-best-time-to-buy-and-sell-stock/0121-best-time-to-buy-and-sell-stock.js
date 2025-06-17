4a8e817dc843706824f3757ccd5b08984e503aad
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-17 11:55:13 +0200

    Time: 2 ms (84.26%), Space: 64 MB (95.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,7 +6,7 @@ var maxProfit = function(prices) {
     let minPrice = prices[0];
     let maxProfit = 0;
 
-    for (let i = 1; i < prices.length; i++) {
+    for (let i = 0; i < prices.length; i++) {
         minPrice = Math.min(minPrice, prices[i]);
         let profit = prices[i] - minPrice;
         maxProfit = Math.max(maxProfit, profit);
