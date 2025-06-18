df2f263d30bc25a1cdec28e2f7a76b01d10800d4
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-18 15:31:57 +0200

    Time: 0 ms (100%), Space: 60.7 MB (32.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -6,18 +6,8 @@ var moveZeroes = function(nums) {
     let p1 = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            nums[p1] = nums[i];
+            [nums[i], nums[p1]] = [nums[p1], nums[i]];
             p1++;
         }
     }
-    
-    for (let i = p1; i < nums.length; i++) {
-        nums[i] = 0;
-    }
 };
-
-// const swap = function(arr, i, j) {
-//     const temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-// }
