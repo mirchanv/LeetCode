38624d1322c462243b56be726c6c6ee7abc276c6
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-18 14:58:18 +0200

    Time: 0 ms (100%), Space: 60 MB (68.19%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -6,19 +6,14 @@ var moveZeroes = function(nums) {
     let p1 = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            nums[p1] = nums[i];
+            swap(nums, p1, i);
             p1++;
         }
     }
-    
-    for (let i = p1; i < nums.length; i++) {
-        console.log(i);
-        nums[i] = 0;
-    }
 };
 
-// const swap = function(arr, i, j) {
-//     const temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-// }
+const swap = function(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
