f79f873cb01bafec2c09bcd2b563a527561aa5b4
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-06-18 13:14:16 +0200

    Time: 0 ms (100%), Space: 54.4 MB (70.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,11 +6,13 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let p1 = m-1;
-    let p2 = n-1;
+    let p1 = m - 1;
+    let p2 = n - 1;
 
     for (let i = m + n -1; i >=0; i--) {
-        if (p2 < 0 || (p1 < m && nums1[p1] > nums2[p2])) {
+        if (p2 < 0) break; 
+        
+        if (p1 < m && nums1[p1] > nums2[p2]) {
             nums1[i] = nums1[p1];
             p1--;
         } else {
